07a501aaa55141c18501578b0c4d85caf8d4e7a2
or-zhang <dwenz94@163.com>
2018-08-29 15:22:14 +0800

    first react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-1 col-xs-offset-11'>
+            <h1>hello world!!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
